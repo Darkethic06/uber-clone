2b8044a61b8a911e109ad664da4b9d2120940798
Darkethic06 <arijitpaul062@gmail.com>
2025-06-25 13:24:04 +0530

    user-register

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,20 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 dotenv.config();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
+
+connectToDb();
 app.use(cors());  
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 app.get('/', (req,res)=>{
     res.send('Hello World');
 });
+
+app.use('/users', userRoutes);
+
 
 module.exports = app;
