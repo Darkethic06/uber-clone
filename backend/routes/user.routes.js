9358075deb87e069f4a5308c78eb1e50d5618901
Darkethic06 <arijitpaul062@gmail.com>
2025-07-01 13:43:59 +0530

    user login

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {body} = require('express-validator');
-const userController = require('../controllers/user.controller');
- 
+const { body } = require("express-validator");
+const userController = require("../controllers/user.controller");
 
-router.post('/register',[
-    body('email').isEmail().withMessage('Invalid Email'),
-    body('fullname.firstname').isLength({min : 3}).withMessage('First name must be at least 3 characterslong'),
-    body('password').isLength({min : 6}).withMessage('Password must be at least 6 chatacters long')
-],userController.registerUser);
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characterslong"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 chatacters long"),
+  ],
+  userController.registerUser
+);
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 chatacters long"),
+  ],
 
+  userController.loginUser
+);
 
 module.exports = router;
